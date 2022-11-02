64f73a6cab726158af37943e27ad53c8f2d128d5
praviny7278 <praviny408@gmail.com>
2022-11-02 16:34:14 +0530

    fixed the code

---
+++ prime.js
@@ -0,0 +1,20 @@
+   function checkprime(num){
+     
+     let count=0;
+
+    for( let i=0;i<=num;i++){
+     
+     if(num%i==0){
+         count++;
+      
+}
+      if(count==9){
+          console.log("Yes");
+      }else{
+          console.log("No");
+      
+}
+}
+
+
+che
